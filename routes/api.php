0cbd52d15ac4d7e530177bed58070bf22edf35f3
RaffyJO <rafiteguh6@gmail.com>
2024-05-01 23:17:33 +0700

    feat: api auth, get doctors and patients

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function ($router) {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/api-doctors', DoctorController::class);
+    Route::apiResource('/api-patients', PatientController::class);
 });
